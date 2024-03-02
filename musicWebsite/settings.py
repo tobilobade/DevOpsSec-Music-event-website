d693e32aebbbce5475874a53b1c219324cffb8d9
EC2 Default User <ec2-user@ip-172-31-39-161.eu-west-1.compute.internal>
2024-03-02 22:42:15 +0000

    collection of static files

--- musicWebsite/settings.py
+++ musicWebsite/settings.py
@@ -122,8 +122,7 @@ STATIC_URL = '/static/'
 
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATIC_ROOT = 'events/staticfiles'
